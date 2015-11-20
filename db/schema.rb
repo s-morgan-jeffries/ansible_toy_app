bd9b175bdcda6421f5ae805fb893563a35fd2805
S Morgan Jeffries <s.morgan.jeffries@gmail.com>
2015-11-20 13:43:20 -0500

    Makes it a todo app

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120073434) do
+ActiveRecord::Schema.define(version: 20151120184052) do
 
   create_table "contacts", force: :cascade do |t|
     t.text     "name"
     t.text     "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "todos", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
